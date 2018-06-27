447aba4e0458337f5ee08a85d1bd7e96de754efb
Leonardo Quixadá <lquixada@gmail.com>
2018-06-26 23:42:47 -0300

    renamed values in the Stack specs.

--- src/__tests__/stack.spec.js
+++ src/__tests__/stack.spec.js
@@ -9,7 +9,7 @@ describe('Stack', () => {
   });
 
   it('starts with elements', () => {
-    const stack = new Stack(['elem1', 'elem2']);
+    const stack = new Stack(['a', 'b']);
 
     expect(stack.size()).toBe(2);
     expect(stack.isEmpty()).toBe(false);
@@ -17,37 +17,37 @@ describe('Stack', () => {
 
   it('adds element', () => {
     const stack = new Stack();
-    stack.push('elem1');
+    stack.push('a');
 
     expect(stack.size()).toBe(1);
   });
 
   it('removes element', () => {
-    const stack = new Stack(['elem1']);
+    const stack = new Stack(['a']);
 
-    expect(stack.pop()).toBe('elem1');
+    expect(stack.pop()).toBe('a');
     expect(stack.size()).toBe(0);
   });
 
   it('removes element in LIFO way', () => {
     const stack = new Stack();
 
-    stack.push('elem1');
-    stack.push('elem2');
-    stack.push('elem3');
+    stack.push('a');
+    stack.push('b');
+    stack.push('c');
 
-    expect(stack.pop()).toBe('elem3');
-    expect(stack.pop()).toBe('elem2');
-    expect(stack.pop()).toBe('elem1');
+    expect(stack.pop()).toBe('c');
+    expect(stack.pop()).toBe('b');
+    expect(stack.pop()).toBe('a');
   });
 
   it('converts to string', () => {
     const stack = new Stack();
 
-    stack.push('elem1');
-    stack.push('elem2');
-    stack.push('elem3');
+    stack.push('a');
+    stack.push('b');
+    stack.push('c');
 
-    expect(stack.toString()).toBe('elem3, elem2, elem1');
+    expect(stack.toString()).toBe('c, b, a');
   });
 });
