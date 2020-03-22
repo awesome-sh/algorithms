e24db46699ed48b5ba615b78f71bb33bb56c3d25
Leonardo Quixadá <lquixada@gmail.com>
2020-03-22 17:24:09 -0400

    fixed merge sort test.

--- src/sorts/__tests__/merge-sort.spec.js
+++ src/sorts/__tests__/merge-sort.spec.js
@@ -1,9 +1,8 @@
 const { mergeSort } = require('../merge-sort')
 
 describe('Merge sort', () => {
-  it('sorts an array in place', () => {
+  it('sorts an array not in place', () => {
     const array = [2, 17, 15, 20, 9, 31]
-    mergeSort(array)
-    expect(array).toEqual([2, 9, 15, 17, 20, 31])
+    expect(mergeSort(array)).toEqual([2, 9, 15, 17, 20, 31])
   })
 })
