13856f92157e5dc41e4390631a77421350d157ad
Leonardo Quixadá <lquixada@gmail.com>
2020-03-23 23:19:11 -0400

    added pop/push digits to gists.

--- src/gists.js
+++ src/gists.js
@@ -2,11 +2,23 @@ function truncate (num) {
   return num | 0
 }
 
+function pop (num) {
+  const digit = num % 10
+  num = (num / 10) | 0
+  return [num, digit]
+}
+
+function push (num, digit) {
+  return num * 10 + digit
+}
+
 function matrix (rows, cols) {
   return Array(rows).fill(Array(cols))
 }
 
 module.exports = {
   matrix,
+  pop,
+  push,
   truncate
 }
