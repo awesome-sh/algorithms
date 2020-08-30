66fbfab4223dd76a112955942addf78da134a3a0
Leonardo Quixadá <lquixada@gmail.com>
2020-08-29 23:16:41 -0400

    removed unused comments.

--- src/data-structure/hashset.ts
+++ src/data-structure/hashset.ts
@@ -43,53 +43,6 @@ class HashSet<T> {
   clear (): void {
     this._items = new HashMap()
   }
-
-  // Operations
-
-  // union(otherSet) {
-  //   const union = new HashSet();
-
-  //   this.values().forEach((value) => union.add(value));
-  //   otherSet.values().forEach((value) => union.add(value));
-
-  //   return union;
-  // }
-
-  // intersect(otherSet) {
-  //   const intersect = new HashSet();
-
-  //   this.values()
-  //     .filter((value) => otherSet.has(value))
-  //     .forEach((value) => intersect.add(value));
-
-  //   return intersect;
-  // }
-
-  // diff(otherSet) {
-  //   const diff = new HashSet();
-
-  //   this.values()
-  //     .filter((value) => !otherSet.has(value))
-  //     .forEach((value) => diff.add(value));
-
-  //   return diff;
-  // }
 }
-
-// let setA = new HashSet();
-// setA.add(1);
-// setA.add(2);
-// setA.add(3);
-
-// let setB = new HashSet();
-// setB.add(2);
-// setB.add(3);
-// setB.add(4);
-// setB.add(5);
-// setB.add(6);
-
-// let unionAB = setB.union(setA);
-
-// console.log(unionAB.values());
 
 export default HashSet
