15767127aa4d1e5d33857c0c9e6e369c29ab95be
Leonardo Quixadá <lquixada@gmail.com>
2021-04-04 14:23:22 -0400

    added small note about Math.sqrt on Closest K Points algorithm.

--- src/algorithms/closest-k-points.ts
+++ src/algorithms/closest-k-points.ts
@@ -31,6 +31,11 @@ export class ClosestKPoints {
   }
 
   computeDist(point: Point, origin: Point): number {
+    /**
+     * Note: Math.sqrt is not needed here because comparing squared node distances will have the same
+     * effect as comparing the non-squared distance. So √4 = √4 does the same thing as 4 = 4. However
+     * we're keeping it to prevent integer overflow.
+     */
     return Math.sqrt(
       Math.pow(point[0] - origin[0], 2) + Math.pow(point[1] - origin[1], 2)
     )
