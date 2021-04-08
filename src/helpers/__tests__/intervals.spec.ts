73574785d397896de00fa1fab4aa3674c006fc6c
Leonardo Quixadá <lquixada@gmail.com>
2021-04-07 23:34:23 -0400

    added overlap logic explanation.

--- src/helpers/__tests__/intervals.spec.ts
+++ src/helpers/__tests__/intervals.spec.ts
@@ -1,4 +1,4 @@
-import { isOverlap, mergeOverlappingIntervals } from '../intervals'
+import { isOverlap, isOverlap2, mergeOverlappingIntervals } from '../intervals'
 
 describe('Intervals', () => {
   describe('isOverlap', () => {
@@ -12,6 +12,28 @@ describe('Intervals', () => {
 
     it('detects a non-overlap', () => {
       expect(isOverlap([1, 3], [4, 7])).toEqual(false)
+    })
+
+    it('detects a touching overlap', () => {
+      expect(isOverlap([1, 3], [3, 7])).toEqual(true)
+    })
+  })
+
+  describe('isOverlap2', () => {
+    it('detects an overlap with ascending start', () => {
+      expect(isOverlap2([1, 5], [4, 7])).toEqual(true)
+    })
+
+    it('detects an overlap with descending start', () => {
+      expect(isOverlap2([4, 7], [1, 5])).toEqual(true)
+    })
+
+    it('detects a non-overlap', () => {
+      expect(isOverlap2([1, 3], [4, 7])).toEqual(false)
+    })
+
+    it('detects a touching overlap', () => {
+      expect(isOverlap2([1, 3], [3, 7])).toEqual(true)
     })
   })
 
--- src/helpers/intervals.ts
+++ src/helpers/intervals.ts
@@ -1,8 +1,42 @@
+/**
+  Determine if there's an overlap between two intervals:
+  First let's think in the opposite direction, how would the intervals look like if they do NOT overlap?
+
+  End time of the first interval has to be earlier than the start time of the second interval.
+
+  x1  x2
+  |---|
+          |----|
+          y1  y2
+          
+  y1  y2
+  |---|
+          |----|
+          x1  x2
+  i.e. (x2 < y1 or y2 < x1)
+
+  So the overlapping condition is the opposite: not (x2 < y1 or y2 < x1)
+*/
 export const isOverlap = (
   a: [number, number],
   b: [number, number]
 ): boolean => {
-  return a[0] < b[1] && b[0] < a[1]
+  return a[0] <= b[1] && b[0] <= a[1]
+  // return !(b[1] < a[0] || a[1] < b[0]);
+}
+
+/**
+  If we want to go one-step further, we can reduce it by DeMorgan's Law: (Not (A or B) == Not A and Not B), this is equivalent to:
+  * not (x2 < y1 or y2 < x1)
+  * -> not (x2 < y1) and not (y2 < x1) (applied DeMorgan's Law)
+  * -> x2 >= y1 and y2 >= x1 (inverted operators)
+  * -> x1 <= y2 and y1 <= x2 (rearranged operands)
+ */
+export const isOverlap2 = (
+  a: [number, number],
+  b: [number, number]
+): boolean => {
+  return a[0] <= b[1] && b[0] <= a[1]
 }
 
 export const mergeOverlappingIntervals = (
